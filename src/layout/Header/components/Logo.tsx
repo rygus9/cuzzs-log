a57fa4c758ff05587681810fe0d1bd8e55aedeea
Cuzz <rygus9@naver.com>
2022-09-04 15:52:09 +0900

    fix: change header link url

--- src/layout/Header/components/Logo.tsx
+++ src/layout/Header/components/Logo.tsx
@@ -1,17 +1,14 @@
+import Link from "next/link";
 import cls from "src/utils/cls";
 
 const Logo = () => {
   return (
-    <div
-      className={cls(
-        "text-2xl font-bold cursor-default tracking-wide",
-        "sm:text-3xl",
-        "hover:animate-pulseFast"
-      )}
-    >
-      <strong className="text-myOrange">Cuzz</strong>
-      {"'"}s LOG
-    </div>
+    <Link href="/">
+      <a className={cls("text-2xl font-bold cursor-pointer tracking-wide", "sm:text-3xl", "hover:animate-pulseFast")}>
+        <strong className="text-myOrange">Cuzz</strong>
+        {"'"}s LOG
+      </a>
+    </Link>
   );
 };
 
--- src/layout/Header/components/Menu.tsx
+++ src/layout/Header/components/Menu.tsx
@@ -1,14 +1,17 @@
 import Link from "next/link";
 import cls from "src/utils/cls";
 
-const menuList = ["POSTS", "ABOUT ME"];
+const menuList = [
+  { link: "/", title: "POSTS" },
+  { link: "/about", title: "ABOUT ME" },
+];
 
 const Menu = () => {
   return (
     <>
       <ul className={cls("flex justify-evenly items-center space-x-4", "sm:text-lg sm:space-x-6")}>
         {menuList.map((elem, index) => (
-          <Link href={"/"} key={index}>
+          <Link href={elem.link} key={index}>
             <li
               className={cls(
                 "py-3 cursor-pointer transition-colors duration-300 text-sm",
@@ -17,7 +20,7 @@ const Menu = () => {
               )}
               key={index}
             >
-              {elem}
+              {elem.title}
             </li>
           </Link>
         ))}
