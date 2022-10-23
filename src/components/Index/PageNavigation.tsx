df60220556ed38c7eaa7ceb23d4bc14f6ba3e4f6
Cuzz <rygus9@naver.com>
2022-10-23 13:31:15 +0900

    fix: remove prefetch option in page navigation & remove scroll cache

--- src/components/Index/PageNavigation.tsx
+++ src/components/Index/PageNavigation.tsx
@@ -10,30 +10,34 @@ interface PageNavigationProps {
 
 const PageNavigation = ({ nowPage, maxPage, baseLink }: PageNavigationProps) => {
   return (
-    <section className="flex items-center justify-between mt-6">
-      <Link href={nowPage == 2 ? baseLink : baseLink + "page/" + (nowPage - 1)} passHref>
-        <a className={nowPage == 1 ? "invisible" : ""}>
-          <PageButton>
-            <div className="flex items-center relative right-0 sm:right-1">
-              <ChevronDoubleLeftIcon className="w-6 h-6 sm:w-8 sm:h-8"></ChevronDoubleLeftIcon>
-              <span className="hidden sm:inline-block">이전 페이지</span>
-            </div>
-          </PageButton>
-        </a>
-      </Link>
-      <div className="justify-center items-center align-top space-x-2 text-xl sm:text-2xl">
+    <section className="relative w-full flex items-center mt-6">
+      {nowPage == 1 || (
+        <Link href={nowPage == 2 ? baseLink : baseLink + "page/" + (nowPage - 1)} passHref>
+          <a className="justify-self-start">
+            <PageButton>
+              <div className="flex items-center relative right-0 sm:right-1">
+                <ChevronDoubleLeftIcon className="w-6 h-6 sm:w-8 sm:h-8"></ChevronDoubleLeftIcon>
+                <span className="hidden sm:inline-block">이전 페이지</span>
+              </div>
+            </PageButton>
+          </a>
+        </Link>
+      )}
+      <div className="absolute left-1/2 -translate-x-1/2 justify-center items-center align-top space-x-2 text-xl sm:text-2xl">
         <span>{nowPage}</span> <span className="text-lg">of</span> <span>{maxPage}</span>
       </div>
-      <Link href={baseLink + "page/" + (nowPage + 1)} passHref>
-        <a className={nowPage == maxPage ? "invisible" : ""}>
-          <PageButton>
-            <div className="flex items-center relative left-0 sm:left-1">
-              <span className="hidden sm:inline-block">다음 페이지</span>
-              <ChevronDoubleRightIcon className="w-6 h-6 sm:w-8 sm:h-8"></ChevronDoubleRightIcon>
-            </div>
-          </PageButton>
-        </a>
-      </Link>
+      {nowPage == maxPage || (
+        <Link href={baseLink + "page/" + (nowPage + 1)} passHref>
+          <a className="inline-block ml-auto">
+            <PageButton>
+              <div className="flex items-center relative left-0 sm:left-1">
+                <span className="hidden sm:inline-block">다음 페이지</span>
+                <ChevronDoubleRightIcon className="w-6 h-6 sm:w-8 sm:h-8"></ChevronDoubleRightIcon>
+              </div>
+            </PageButton>
+          </a>
+        </Link>
+      )}
     </section>
   );
 };
