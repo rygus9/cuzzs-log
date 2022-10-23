c06879416c986cb9d20add2c2feae028d2a36d13
Cuzz <rygus9@naver.com>
2022-10-23 17:37:22 +0900

    feat: add footer github icon

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,5 +1,12 @@
+import GithubIcon from "src/components/common/Icon/GithubIcon";
+
 const Footer = () => (
   <footer className="pt-6 pb-12 text-center border-t border-gray-600">
+    <div className="flex items-center justify-center mb-2">
+      <a href="https://github.com/rygus9" target="blank">
+        <GithubIcon></GithubIcon>
+      </a>
+    </div>
     By <span className="text-myOrange text-xl ml-2">Cuzz</span>
   </footer>
 );
