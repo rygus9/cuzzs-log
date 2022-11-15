bcb1d117fc71baf5ab8df9a4c5b1b1d13716067a
Cuzz <rygus9@naver.com>
2022-11-15 15:09:44 +0900

    fix: fix post bundling

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 //   enabled: process.env.ANALYZE === "true",
 // });
 // module.exports = withBundleAnalyzer({});
-const withTM = require("next-transpile-modules")(["react-syntax-highlighter"]); // pass the modules you would like to see transpiled
+const withTM = require("next-transpile-modules")(["react-syntax-highlighter"]);
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
